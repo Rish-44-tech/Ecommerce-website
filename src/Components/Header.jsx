4a202811299e957726407854d4efd0643a9c501b
Rishit <jalanrishit@gmail.com>
2026-06-16 16:05:47 +0000

    made search bar responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,25 @@
-import { Link } from "react-router";
+import { Link, useNavigate, useSearchParams } from "react-router";
+import { useState } from "react";
 import "./Header.css";
 import logo_white from "../assets/images/logo-white.png";
 import mob_logo_white from "../assets/images/mobile-logo-white.png";
 import cart_icon from "../assets/images/icons/cart-icon.png";
 import search from "../assets/images/icons/search-icon.png";
 
-export default function Header({cart}) {
-  let totalQuantity=0;
-  Object.entries(cart).forEach((cart_item)=>{
-    totalQuantity+=cart_item[1].quantity;
-  })
+export default function Header({ cart }) {
+  const nav = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const searches = searchParams.get("search");
+
+  const [searchData, setSearchData] = useState(searches || null);
+
+  let totalQuantity = 0;
+
+  Object.entries(cart).forEach((cart_item) => {
+    totalQuantity += cart_item[1].quantity;
+  });
+
   return (
     <div className="header">
       <div className="left-section">
@@ -20,9 +30,27 @@ export default function Header({cart}) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          value={searchData || ""}
+          onChange={(event) => {
+            setSearchData(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              nav(`/?search=${searchData}`);
+            }
+          }}
+        />
 
-        <button className="search-button">
+        <button
+          className="search-button"
+          onClick={() => {
+            nav(`/?search=${searchData}`);
+          }}
+        >
           <img className="search-icon" src={search} />
         </button>
       </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,35 @@
 import axios from "axios";
+import { useSearchParams } from "react-router";
 import { useEffect, useState } from "react";
 import Header from "../../Components/Header";
 import "./Home.css";
 import ProductsGrid from "./ProductsGrid";
 
-export default function Home({ cart,loadCart }) {
+export default function Home({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
-  
+
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
+
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get("/api/products");
-      setProducts(response.data);
+      if (search) {
+        const response=await axios.get(`/api/products?search=${search}`);
+        setProducts(response.data);
+      } else {
+        const response = await axios.get("/api/products");
+        setProducts(response.data);
+      }
     };
     getHomeData();
-  }, []);
+  }, [search]);
 
   return (
     <>
       <link rel="icon" href="images/home-favicon.png" />
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} loadCart={loadCart}/>
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
